8de415732e057921fdfc3bd93d7e3dbf50694cf9
nuggsrocks <nuggsrocks@yahoo.com>
2021-07-18 02:13:15 -0400

    correct dtypes for pandas

--- boxscores.py
+++ boxscores.py
@@ -1,31 +1,11 @@
+import datetime
 import re
 import requests
-import datetime
-from calendar import Calendar
 from bs4 import BeautifulSoup
 import pandas as pd
-from pandas.core.common import flatten
 
 
-def create_date_list():
-    c = list(flatten(Calendar().yeardatescalendar(2020)))
-    c.extend(list(flatten(Calendar().yeardatescalendar(2021))))
-
-    season_start_date = datetime.date(year=2020, month=12, day=22)
-    season_end_date = datetime.date(year=2021, month=5, day=16)
-
-    c = pd.Series(c).drop_duplicates()
-
-    c = c.loc[lambda x: x >= season_start_date]
-
-    c = c.loc[lambda x: x <= season_end_date]
-
-    c = c.reset_index(drop=True)
-
-    return list(c)
-
-
-def scrape_for_stats(date):
+def scrape_for_boxscores(date):
     url = 'https://www.basketball-reference.com/boxscores/'
 
     params = {
@@ -55,33 +35,60 @@ def scrape_for_stats(date):
         for tag in soup.find_all(string=re.compile('Basic Box Score')):
             for parent in tag.parents:
                 if parent.name == 'table' and re.search('game', parent['id']) is not None:
-                    team_code = re.search('[A-Z]{3}', parent['id']).group()
                     stat_df = pd.read_html(str(parent), header=1)[0]
-                    stat_df.rename(columns={'Starters': 'Name'}, inplace=True)
-                    stat_df.drop(index=[5], inplace=True)
-                    team_totals_index = stat_df['Name'][lambda x: x == 'Team Totals'].index[0]
-                    stat_df.drop(index=team_totals_index, inplace=True)
-                    stat_dfs[team_code] = stat_df
+
+                    processed_df = stat_df.rename(columns={'Starters': 'NAME'})
+                    processed_df = processed_df.drop(index=[5])
+                    team_totals_index = processed_df['NAME'][lambda x: x == 'Team Totals'].index[0]
+                    processed_df = processed_df.drop(index=team_totals_index)
+
+                    dnp_indexes = processed_df.loc[processed_df['MP'] == 'Did Not Play'].index
+
+                    for index in dnp_indexes:
+                        processed_df = processed_df.drop(index=index)
+
+                    processed_df = processed_df.drop(['FG%', '3P%', 'FT%'], axis=1)
+
+                    team = re.search('[A-Z]{3}', parent['id']).group()
+                    stat_dfs[team] = processed_df
 
         boxscores.append(stat_dfs)
 
     return boxscores
 
 
-dates = create_date_list()
+season_start_date = datetime.date(year=2020, month=12, day=22)
+season_end_date = datetime.date(year=2021, month=5, day=16)
+
+dates = pd.date_range(start=season_start_date, end=season_end_date, freq='D')
 
 full_df = pd.DataFrame()
 
 game_index = 0
 
 for date in dates:
-    games = scrape_for_stats(date)
+    games = scrape_for_boxscores(date)
 
-    for game in games:
-        for team_code in game:
-            game[team_code]['GAME_ID'] = game_index
-            game[team_code]['TEAM'] = team_code
-            full_df = full_df.append(game[team_code], ignore_index=True)
+    for boxscore in games:
+        for team_code in boxscore:
+            boxscore[team_code]['GAME_ID'] = game_index
+            boxscore[team_code]['TEAM'] = team_code
+            full_df = full_df.append(boxscore[team_code], ignore_index=True)
         game_index += 1
 
+
+def set_dtypes(series):
+    if series.name == 'NAME' or series.name == 'TEAM':
+        return series.astype('string')
+    elif series.name == 'MP':
+        time_list = list(series.str.split(':'))
+
+        timedelta_list = list(map(lambda x: pd.Timedelta(minutes=int(x[0]), seconds=int(x[1])), time_list))
+
+        return pd.Series(timedelta_list)
+    else:
+        return series.astype('int64')
+
+
+full_df = full_df.apply(set_dtypes)
 print(full_df)
